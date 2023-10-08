1ee4d81d64d015d42e474e6c7edad2e80cb35efb
hugoa <hugoa@192.168.1.145>
2023-10-08 19:36:38 +0200

    Fist Commit with initial project

---
+++ LocalSearch/src/IA/Bicing/BicingBoard.java
@@ -0,0 +1,17 @@
+package IA.Bicing;
+
+import java.util.*;
+
+public class BicingBoard {
+
+	/// Numero de bicis
+	private int nbikes;
+	
+	/// Numero de estaciones
+	private int nstations;
+	
+	/// Lista con las estaciones que nos da el enunciado
+	private Estaciones stations;
+	
+	
+}
