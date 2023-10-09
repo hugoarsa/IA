fd7045b789517257ef8ba6ca3d746e5a5262c013
hugoa <hugoa@192.168.1.146>
2023-10-09 19:19:08 +0200

    Añadidas funciones auxiliares de distancia

--- LocalSearch/src/IA/Bicing/BicingBoard.java
+++ LocalSearch/src/IA/Bicing/BicingBoard.java
@@ -1,35 +1,79 @@
 package IA.Bicing;
 
-import javafx.util.Pair;  // Importa la clase Pair
 
 
 public class BicingBoard {
 
 	/// Numero de bicis
-	static private int nbikes;
+	private int nbikes;
 	
 	/// Numero de estaciones
-	static private int nstations;
+	private int nstations;
 	
-	static private int ntrucks;
+	private int ntrucks;
 	
 	/// Lista con las estaciones que nos da el enunciado
 	static private Estaciones stations;
 	
+	static private int[][] distances;
+	
 	/// Vector con las rutas posibles
 	private Route[] routes;
 	
-	private Pair<Integer, Boolean>[] meta_stations;
+	/// Vector que nos dice si las estaciones son de inicio
+	private Boolean[] start_stations;
+	
+	/// Vector que nos dice como van de bicis las estaciones
+	private int[] state_stations;
 
 
-    public BicingBoard(int nbikes, int nstations, Estaciones stations) {
-        this.nbikes = nbikes;
-        this.nstations = nstations;
-        this.stations = stations;
+    public BicingBoard(Estaciones e, int nb, int nt) {
+        nbikes = nb;
+        ntrucks = nt;
+        nstations = e.size();
+        stations = e;
         
         // Inicializar los vectores de utilización y recorridos con el tamaño adecuado
-        routes = new Route[ntrucks];
-        meta_stations = new Pair<Integer, Boolean>[nstations];
+        routes = new Route[nt];
+        
+        start_stations = new Boolean[nstations];
+        
+        // inicializar las stations
+        
+        state_stations = new int[nstations];
+        
+        // inicializar distances
+        
+        distances = calculateDistanceMatrix(e);
+    }
+    
+    /*!\brief Calcula la distancia entre dos estaciones
+    *
+    * @param [i] Estacion i
+    * @param [j] Estacion j
+    */
+    public static int calculateDistance(Estacion i, Estacion j) {
+        int ix = i.getCoordX();
+        int iy = i.getCoordY();
+        int jx = j.getCoordX();
+        int jy = j.getCoordY();
+
+        // Calculate the distance using the formula d(i, j) = |ix − jx| + |iy − jy|
+        int distance = Math.abs(ix - jx) + Math.abs(iy - jy);
+        return distance;
+    }
+    
+    public static int[][] calculateDistanceMatrix(Estaciones e) {
+        int numStations = e.size();
+        int[][] distanceMatrix = new int[numStations][numStations];
+
+        for (int i = 0; i < numStations; i++) {
+            for (int j = 0; j < numStations; j++) {
+                distanceMatrix[i][j] = calculateDistance(e.get(i), e.get(j));
+            }
+        }
+
+        return distanceMatrix;
     }
 }
 
